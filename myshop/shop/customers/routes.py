342bcc2d639856e6eeba37f402749d1418d38c3c
Ilyes <ilyessellam04@gmail.com>
2022-04-28 16:57:57 +0100

    add SES AWS service to register and login customers routes

--- myshop/shop/customers/routes.py
+++ myshop/shop/customers/routes.py
@@ -1,11 +1,16 @@
+
 from flask import render_template, url_for, flash, redirect, Blueprint, request
 from shop import db, bcrypt
 from shop.customers.forms import (RegistrationFrom, LoginFrom, UpdateAccountFrom)
 from shop.models import Customer, Categorie
 from flask_login import login_user, current_user, logout_user, login_required
+import boto3
 
 
 customers = Blueprint('customers', __name__)
+client = boto3.client('ses')
+kms = boto3.client('kms', region_name='us-east-2')
+
 
 @customers.route("/customer_register", methods=['GET', 'POST'])
 def customer_register():
@@ -16,15 +21,13 @@ def customer_register():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
             form.password.data).decode('utf-8')
-        try:
-            customer = Customer(username=form.username.data,
-                        email=form.email.data, address=form.address.data, password=hashed_password)
-            db.session.add(customer)
-            db.session.commit()
-        except Exception as e:
-            return flash(f'internal error server', 'success')
+        customer = Customer(username=form.username.data,
+                    email=form.email.data, address=form.address.data, password=hashed_password)
+        client.verify_email_address(EmailAddress=customer.email)
+        db.session.add(customer)
+        db.session.commit()
 
-        flash(f'Your account has been created! You are now able to log in', 'success')
+        flash(f'You should confirm your email address! Befor log in', 'success')
         return redirect(url_for('customers.customer_login'))
     return render_template('customer_register.html', title='Register', form=form, categories=categories)
 
@@ -38,9 +41,17 @@ def customer_login():
     if form.validate_on_submit():
         customer = Customer.query.filter_by(email=form.email.data).first()
         if customer and bcrypt.check_password_hash(customer.password, form.password.data):
-            login_user(customer, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+            response = client.get_identity_verification_attributes(
+                       Identities=[customer.email]
+                                            )
+            res = response['VerificationAttributes'][customer.email]['VerificationStatus']
+            if res == 'Success':
+                login_user(customer, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('main.home'))
+            else:
+                flash(f'You should confirm your email address! Befor log in', 'success')
+                return redirect(url_for('customers.customer_login'))
         else:
             flash('log in unsuccessful, please check username and password', 'danger')
     return render_template('customer_login.html', title='Login', form=form, categories=categories)
